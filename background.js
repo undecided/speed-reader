390dc5449b4bef5fe4c1e7cb30d110b5ec82587b
Matthew Bennett-Lovesey <matt+gitcommits@blltd.uk>
2024-03-06 12:46:50 +0000

    Iconic

--- background.js
+++ background.js
@@ -1,14 +1,16 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({
-    text: "OFF",
+    text: "",
   });
 });
 
 chrome.action.onClicked.addListener(async (tab) => {
-  // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+  const ON = "✅";
+  const OFF = "";
+  // Retrieve the action badge to check if the extension is ON or OFF
   const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
   // Next state will always be the opposite
-  const nextState = prevState === 'ON' ? 'OFF' : 'ON'
+  const nextState = prevState === ON ? OFF : ON
 
   // Set the action badge to the next state
   await chrome.action.setBadgeText({
@@ -19,7 +21,7 @@ chrome.action.onClicked.addListener(async (tab) => {
   chrome.scripting
     .executeScript({
       target : {tabId : tab.id},
-      files : [ nextState == "ON" ? "speedreader.js" : "speedreader_off.js" ],
+      files : [ nextState == ON ? "speedreader.js" : "speedreader_off.js" ],
     }).then(() => console.log("injected script file"));
 });
 
